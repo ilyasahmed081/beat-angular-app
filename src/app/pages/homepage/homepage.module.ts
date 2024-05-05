8e9d1b27b64d44dcf6deb48fc7f602b526f2242b
ilyasahmed081 <ilyasahmed081@gmail.com>
2024-05-05 23:14:34 +0500

    carousel work init

--- src/app/pages/homepage/homepage.module.ts
+++ src/app/pages/homepage/homepage.module.ts
@@ -4,11 +4,13 @@ import { CommonModule } from '@angular/common';
 import { HomepageRoutingModule } from './homepage-routing.module';
 import { HomepageComponent } from './homepage.component';
 import { TopBarComponent } from 'src/app/layout/top-bar/top-bar.component';
+import { CarouselComponent } from 'src/app/components/carousel/carousel.component';
 
 @NgModule({
   declarations: [
     HomepageComponent,
-    TopBarComponent
+    TopBarComponent,
+    CarouselComponent
   ],
   imports: [
     CommonModule,
