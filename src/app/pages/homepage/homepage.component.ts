5d0b8a43d7f73013d32c87f104e3fa1a7788c673
ilyasahmed081 <ilyasahmed081@gmail.com>
2024-05-07 23:39:47 +0500

    information card init

--- src/app/pages/homepage/homepage.component.ts
+++ src/app/pages/homepage/homepage.component.ts
@@ -7,4 +7,99 @@ import { Component } from '@angular/core';
 })
 export class HomepageComponent {
 
+  category: any = {
+    title: 'Popular Categories',
+    categories: [
+      { name: 'Modern Edition' },
+      { name: 'Professional Edition' },
+      { name: 'Sport Edition' },
+      { name: 'Classic Edition' },
+      { name: 'Progressive Edition' },
+      { name: 'Color Edition' }
+    ]
+  }
+  // product category cards
+  productCategory: any = [
+    {
+      id: 1,
+      img: '../../../assets/img/headphone2.png',
+      title: 'Fatex Product Headphone',
+      info: 'There are many variations of passages of Lorem Ipsum, but the suffered',
+      price: '$110.00',
+    },
+    {
+      id: 2,
+      img: '../../../assets/img/headphone2.png',
+      title: 'Fatex Product Headphone',
+      info: 'There are many variations of passages of Lorem Ipsum, but the suffered',
+      price: '$110.00',
+    },
+    {
+      id: 3,
+      img: '../../../assets/img/headphone2.png',
+      title: 'Fatex Product Headphone',
+      info: 'There are many variations of passages of Lorem Ipsum, but the suffered',
+      price: '$110.00',
+    },
+  ];
+
+  // best products
+  bestProducts: any = [
+    {
+      id: 1,
+      img: '../../../assets/img/headphone2.png',
+      title: 'Fatex Product Headphone',
+      info: 'There are many variations of passages of Lorem Ipsum, but the suffered',
+      price: '$110.00',
+    },
+    {
+      id: 2,
+      img: '../../../assets/img/headphone2.png',
+      title: 'Fatex Product Headphone',
+      info: 'There are many variations of passages of Lorem Ipsum, but the suffered',
+      price: '$110.00',
+    },
+    {
+      id: 3,
+      img: '../../../assets/img/headphone2.png',
+      title: 'Fatex Product Headphone',
+      info: 'There are many variations of passages of Lorem Ipsum, but the suffered',
+      price: '$110.00',
+    },
+    {
+      id: 4,
+      img: '../../../assets/img/headphone2.png',
+      title: 'Fatex Product Headphone',
+      info: 'There are many variations of passages of Lorem Ipsum, but the suffered',
+      price: '$110.00',
+    },
+    {
+      id: 5,
+      img: '../../../assets/img/headphone2.png',
+      title: 'Fatex Product Headphone',
+      info: 'There are many variations of passages of Lorem Ipsum, but the suffered',
+      price: '$110.00',
+    },
+    {
+      id: 6,
+      img: '../../../assets/img/headphone2.png',
+      title: 'Fatex Product Headphone',
+      info: 'There are many variations of passages of Lorem Ipsum, but the suffered',
+      price: '$110.00',
+    },
+    {
+      id: 7,
+      img: '../../../assets/img/headphone2.png',
+      title: 'Fatex Product Headphone',
+      info: 'There are many variations of passages of Lorem Ipsum, but the suffered',
+      price: '$110.00',
+    },
+    {
+      id: 8,
+      img: '../../../assets/img/headphone2.png',
+      title: 'Fatex Product Headphone',
+      info: 'There are many variations of passages of Lorem Ipsum, but the suffered',
+      price: '$110.00',
+    }
+  ];
 }
